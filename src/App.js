2f9c18da1e9dad7e2b3fe42f2bfcc11d3e25ee91
MuhammadHafijur <hafijur.salah@gmail.com>
2021-10-29 12:01:08 +0600

    added header and banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Pages/Shared/Header/Header";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hover:text-green-400">This is tailwindcss</h1>
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './airplane.png'
+
+const Header = () => {
+    return (
+        <header className="text-gray-600 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <Link to='/home' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src={logo} alt="" />
+      <span className="ml-3 text-xl">JustGo</span>
+    </Link>
+    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <Link to="/home" className="mr-5 hover:text-gray-900">Home</Link>
+      <Link to="/about" className="mr-5 hover:text-gray-900">Services</Link>
+      <Link to="/contact" className="mr-5 hover:text-gray-900">Contact</Link>
+      <Link to="/help" className="mr-5 hover:text-gray-900">Help</Link>
+    </nav>
+    <Link to="/login" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+    </Link>
+  </div>
+</header>
+    );
+};
+
+export default Header;
